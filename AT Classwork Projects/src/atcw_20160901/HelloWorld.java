2757e0917a40cad63d5ce2b85889e2dfc3e2fb54
Louis Minsky <lminsky@packer.edu>
2016-11-21 15:05:51 -0500

    added a comment

--- AT Classwork Projects/src/atcw_20160901/HelloWorld.java
+++ AT Classwork Projects/src/atcw_20160901/HelloWorld.java
@@ -3,8 +3,7 @@ package atcw_20160901;
 public class HelloWorld {
 	public static void main(String[] args) {
 		String s="Hello";
-		System.out.println("Hello World");
-		
+		System.out.println("Hello World"); //This prints out "Hello World"
 		/*
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter a Number: ");
