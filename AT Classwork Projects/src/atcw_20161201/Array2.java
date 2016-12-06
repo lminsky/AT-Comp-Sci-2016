2b4248a8cb962fd76b7c4b5e87ac5bf504c4f9ff
gbenedisgrab <gbenedisgrab@packer.edu>
2016-12-06 08:50:46 -0500

    added main method

--- AT Classwork Projects/src/atcw_20161201/Array2.java
+++ AT Classwork Projects/src/atcw_20161201/Array2.java
@@ -1,5 +1,8 @@
 package atcw_20161201;
 
 public class Array2 {
-
+	int[] nums = new int[0];
+	Array2() {}
+	void push(int x) {}
+	
 }
--- AT Classwork Projects/src/atcw_20161201/UsingArray2.java
+++ AT Classwork Projects/src/atcw_20161201/UsingArray2.java
@@ -1,5 +1,7 @@
 package atcw_20161201;
 
 public class UsingArray2 {
-
+	public static void main(String args[]){
+		
+	}
 }
