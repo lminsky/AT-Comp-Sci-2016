5471574cac58799e4e3d54febe4127be2e8d02a9
Louis Minsky <lminsky@packer.edu>
2017-01-09 14:25:53 -0500

    created write.java

---
+++ AT Classwork Projects/src/atcs_20170109/write.java
@@ -0,0 +1,30 @@
+package atcs_20170109;
+
+import java.io.FileWriter;
+
+public class write {
+
+  public static void main(String[] args){
+    FileWriter fw;
+    try {
+      fw = new FileWriter("out.txt");
+      String s = "";
+      
+      for(int i = 0; i < 5; i++) {
+    	  	s += (int)(Math.random()*100);
+    	  	if(i < 4)
+    	  		s += ",";
+      }
+      
+      System.out.println(s);
+      
+      fw.write(s);
+      
+      fw.close();
+    } catch (Exception e) {
+      System.out.println("There was an error");
+    }
+     
+  }
+
+}
