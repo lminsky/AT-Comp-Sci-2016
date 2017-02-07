a417fc7fd063397b7e25e2da935f90e1111d19ac
Louis Minsky <lminsky@packer.edu>
2017-02-07 12:09:49 -0500

    added compareTo with strings

---
+++ AT Classwork Projects/src/atcw_20170207/compareTo.java
@@ -0,0 +1,15 @@
+package atcw_20170207;
+
+public class compareTo {
+
+	public static void main(String[] args) {
+		String a = "aaaaaaaaaaaaaaaaaaaaaaaa aa";
+		String b = "aaaaaaaaaaaaaaaaaaaaaaaa aa";
+		
+		if(a == b) {
+			System.out.println("YES");
+		}
+
+	}
+
+}
